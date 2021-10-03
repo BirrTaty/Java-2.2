8a7ea46a641910b5df6cd69394eabc547e0d5bfa
Tatiana <birrtaty@gmail.com>
2021-10-03 22:22:22 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
+import java.sql.SQLOutput;
+
 public class Main {
+    public static void main(String[] args) {
+        int moneyOnTheAccount =100;
+        int refill = 2000;
+        int account = moneyOnTheAccount + refill;
+        int bonus;
+
+        System.out.println("Итоговый счет: " + account);
+
+        if (account > 1000) {
+            bonus = account/100;
+        }else {
+            bonus = 0;
+        }
+        System.out.println("Количество бонусов: " + bonus);
+
+    }
+
 }
